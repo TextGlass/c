0a4984a4ffd432196971805da0d5503677af073c
Reza Naghibi <reza.naghibi@yahoo.com>
2015-08-11 18:54:57 -0400

    optarg

--- src/main.c
+++ src/main.c
@@ -1,38 +1,30 @@
 #include "textglass.h"
 #include "list.h"
 
+#include <unistd.h>
+
 static void printHelp()
 {
-	printf("Usage: textglass_client [OPTIONS] [STRING]\n");
+	printf("Usage: textglass_client [OPTIONS]\n");
 	printf("  -p <file>            load TextGlass pattern file (REQUIRED)\n");
 	printf("  -a <file>            load TextGlass attribute file\n");
-	printf("  -pp <file>           load TextGlass pattern patch file\n");
-	printf("  -ap <file>           load TextGlass attribute patch file\n");
+	printf("  -q <file>            load TextGlass pattern patch file\n");
+	printf("  -b <file>            load TextGlass attribute patch file\n");
 	printf("  -t <file>            load TextGlass test file\n");
 	printf("  -h                   print help\n");
-	printf("  STRING               test string\n");
+	printf("  -u <string>          test string\n");
 }
 
-static char *getParam(int argc, char **args, int pos)
-{
-	if(pos >= argc) {
-		return NULL;
-	} else if(args[pos][0] == '-' || !args[pos][0]) {
-		return NULL;
-	} else {
-		return args[pos];
-	}
-}
-
-int main(int argc, char **args)
+int main(int argc, char **argv)
 {
 	tg_list *tests;
+	tg_list_item *item;
 	char *pattern = NULL;
-	//char *attribute = NULL;
-	//char *patternPatch = NULL;
-	//char *attributePatch = NULL;
-	//char *testString = NULL;
-	int i, exit = 0;
+	char *attribute = NULL;
+	char *patternPatch = NULL;
+	char *attributePatch = NULL;
+	char *testString = NULL;
+	int c, exit = 0;
 
 	printf("TextGlass C Client %s\n", TEXTGLASS_VERSION);
 
@@ -40,27 +32,37 @@ int main(int argc, char **args)
 
 	//PARSE THE COMMAND LINE
 
-	for(i = 1; i < argc; i++) {
-		if(!strcmp(args[i], "-h")) {
-			printHelp();
-			goto mdone;
-		} else if(!strcmp(args[i], "-p")) {
-			if(pattern) {
-				fprintf(stderr, "pattern file already defined\n");
+	opterr = 0;
+
+	while ((c = getopt(argc, argv, "p:a:q:b:t:hu:")) != -1) {
+		switch (c)
+		{
+			case 'h':
+				printHelp();
+				goto mdone;
+			case 'p':
+				pattern = optarg;
+				break;
+			case 'a':
+				attribute = optarg;
+				break;
+			case 'q':
+				patternPatch = optarg;
+				break;
+			case 'b':
+				attributePatch = optarg;
+				break;
+			case 't':
+				tg_list_add(tests, optarg);
+				break;
+			case 'u':
+				testString = optarg;
+				break;
+			default:
+				printHelp();
+				fprintf(stderr, "\nunknown option: %c\n", optopt);
 				exit = 1;
 				goto mdone;
-			}
-			if(!(pattern = getParam(argc, args, ++i)))
-			{
-				fprintf(stderr, "-p parameter missing\n");
-				exit = 1;
-				goto mdone;
-			}
-		} else {
-			printHelp();
-			fprintf(stderr, "\nunknown option: %s\n", args[i]);
-			exit = 1;
-			goto mdone;
 		}
 	}
 
@@ -72,6 +74,31 @@ int main(int argc, char **args)
 	}
 
 	printf("Pattern file: %s\n", pattern);
+
+	if(attribute)
+	{
+		printf("Attribute file: %s\n", attribute);
+	}
+
+	if(patternPatch)
+	{
+		printf("Pattern patch file: %s\n", patternPatch);
+	}
+
+	if(attributePatch)
+	{
+		printf("Attribute patch file: %s\n", attributePatch);
+	}
+
+	tg_list_foreach(tests, item)
+	{
+		printf("Test file: %s\n", (char*)item->value);
+	}
+
+	if(testString)
+	{
+		printf("Test string: '%s'\n", testString);
+	}
 
 mdone:
 	tg_list_free(tests);
